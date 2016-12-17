e2ce11d70ca37b169072373059acf66b286d21bc
root <born2net@gmail.com>
2016-12-17 07:56:54 -0800

    added Staggered

---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+var ng_mslib_1 = require("./ng-mslib");
+exports.Ngmslib = ng_mslib_1.Ngmslib;
+var actions_1 = require("./actions");
+exports.Actions = actions_1.Actions;
+//# sourceMappingURL=index.js.map
