dda4dd3c2cfc7fca5b6b4cfca4a607f49afbc194
root <born2net@gmail.com>
2017-01-04 13:41:38 -0800

    added GlobalizeStringJS in forRoot

--- src/mslib.module.ts
+++ src/mslib.module.ts
@@ -2,6 +2,7 @@ import {NgModule, ModuleWithProviders} from "@angular/core";
 import {MyNgComp} from "./myng-component";
 import {StringJSPipe} from "./pipes/stringjs.pipe";
 import {CommonModule} from "@angular/common";
+import {Ngmslib} from "./ng-mslib";
 
 // if you were ever to add a provided Service feature, it needs to have it's own module,
 // since it requires a single import only from within  app.module, contrary to
@@ -17,6 +18,7 @@ export const MSLIB_DIRECTIVES: Array<any> = [MyNgComp, StringJSPipe];
 
 export class MsLibModule {
     static forRoot(): ModuleWithProviders {
+        Ngmslib.GlobalizeStringJS();
         return {
             ngModule: MsLibModule,
             providers: []
