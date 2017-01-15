09b855d5c4de2eda23251ac5bec4a6eafaa3d921
root <born2net@gmail.com>
2017-01-14 18:46:11 -0800

    switched to ngc from tsc

--- src/mslib.module.ts
+++ src/mslib.module.ts
@@ -7,6 +7,7 @@ import {Ngmslib} from "./ng-mslib";
 // if you were ever to add a provided Service feature, it needs to have it's own module,
 // since it requires a single import only from within  app.module, contrary to
 // feature modules which get imported multiple times by other modules
+// to see module providing module see: https://github.com/Toxicable/angular-validators/blob/master/src/validation-messages/validation-messages.module.ts
 
 export const MSLIB_DIRECTIVES: Array<any> = [MyNgComp, StringJSPipe];
 
