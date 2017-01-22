cd1c621da12c1d3d4065aecdaa3aa40532863c16
root <born2net@gmail.com>
2017-01-22 14:52:53 -0800

    added skipTemplateCodegen

--- src/components/myng-component.ts
+++ src/components/myng-component.ts
@@ -10,7 +10,7 @@ import {Component} from "@angular/core";
 
 export class MyNgComp {
     constructor() {
-        console.log('>>>>> myng-component v86');
+        console.log('>>>>> myng-component v95');
         console.log('Hello from MyNgComp')
     }
 
